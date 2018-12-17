477991ba42297784d5e3c687d3df2efaf93c6aa4
小虎哥 <529901956@qq.com>
2018-12-18 00:10:41 +0800

    EyouCMS-V1.2.2-UTF8-SP1

---
+++ application/lang/en.php
@@ -0,0 +1,13 @@
+<?php
+
+return array (
+  'sys1' => 'Home',
+  'sys2' => 'Previous page',
+  'sys3' => 'Next page',
+  'sys4' => 'Last',
+  'sys5' => 'Road <strong>%s</strong> page <strong>%s</strong> strip',
+  'sys6' => 'Whole',
+  'sys7' => 'Search',
+  'sys8' => 'View details',
+  'sys9' => 'Web home',
+);
